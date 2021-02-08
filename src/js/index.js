e96026bc14e0163125ce37a2decb52f0f52d0d07
Ayush Chugh <ayushchugh2006@gmail.com>
2021-02-09 03:42:25 +0530

    done show_hide

---
+++ src/js/index.js
@@ -0,0 +1,51 @@
+const loginBtn = document.querySelector('.btn-nav__login');
+const signupBtn = document.querySelector('.btn-nav__signup');
+const crossLogin = document.querySelector('.cross-login');
+const crossSignup = document.querySelector('.cross-signup');
+const loginFormContainer = document.querySelector('.login-form-container');
+const signupFormContainer = document.querySelector('.signup-form-container');
+const welcomeBtn = document.querySelector('.welcome__btn');
+const footerBtn = document.querySelector('.footer__btn');
+const formOverlayLinkLogin = document.querySelector(
+	'.form-overlay__link-login'
+);
+const formOverlayLinkSignup = document.querySelector(
+	'.form-overlay__link-signup'
+);
+
+function showLoginForm() {
+	loginFormContainer.classList.remove('u-invisible');
+}
+
+function hideLoginForm() {
+	loginFormContainer.classList.add('u-invisible');
+}
+
+function showSighupForm() {
+	signupFormContainer.classList.remove('u-invisible');
+}
+
+function hideSignupForm() {
+	signupFormContainer.classList.add('u-invisible');
+}
+
+function showHideSignup() {
+	loginFormContainer.classList.remove('u-invisible');
+	signupFormContainer.classList.add('u-invisible');
+}
+
+function showHideLogin() {
+	loginFormContainer.classList.add('u-invisible');
+	signupFormContainer.classList.remove('u-invisible');
+}
+
+loginBtn.addEventListener('click', showLoginForm);
+signupBtn.addEventListener('click', showSighupForm);
+crossLogin.addEventListener('click', hideLoginForm);
+crossLogin.addEventListener('click', hideSignupForm);
+crossSignup.addEventListener('click', hideLoginForm);
+crossSignup.addEventListener('click', hideSignupForm);
+welcomeBtn.addEventListener('click', showSighupForm);
+footerBtn.addEventListener('click', showSighupForm);
+formOverlayLinkLogin.addEventListener('click', showHideSignup);
+formOverlayLinkSignup.addEventListener('click', showHideLogin);
