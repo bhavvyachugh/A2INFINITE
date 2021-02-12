c55033ad44a7636680cb917f2ab2d0bbc5bab7ae
Bhavya Chugh <bhavvyachugh@gmail.com>
2021-02-13 00:03:49 +0530

    solved bug of html file in contact us

--- a2infinity/home/views.py
+++ a2infinity/home/views.py
@@ -66,5 +66,15 @@ def handleLogout(request):
     return redirect('home')
     return HttpResponse('logout')
 
+
 def contact(request):
-   return render(request,"contact.html")
+    if request.method == "POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        desc = request.POST.get('desc')
+        contact = Contact(name=name, email=email, phone=phone, desc=desc, date = datetime.today())
+        contact.save()
+        messages.success(request, 'Your message has been sent!')
+    return render(request, 'contact.html')
+ 
