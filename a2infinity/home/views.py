8809388949ae1710ca11f7942c0f0d4fb904ab64
Ayush Chugh <ayushchugh2006@gmail.com>
2021-02-13 23:31:26 +0530

    done sass for contact-us

--- a2infinity/home/views.py
+++ a2infinity/home/views.py
@@ -78,6 +78,6 @@ def contact(request):
         desc = request.POST.get('desc')
         contact = Contact(name=name, email=email, phone=phone, desc=desc, date = datetime.today())
         contact.save()
-        messages.success(request, 'Your message has been sent!')
+        messages.success(request, 'Your message has been sent !')
     return render(request, 'contact.html')
  
