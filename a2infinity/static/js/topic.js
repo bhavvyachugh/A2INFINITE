5feffe3bd0576f3b25f73a616e817fe39fe464e1
Ayush Chugh <ayushchugh2006@gmail.com>
2021-02-25 12:53:32 +0530

    submiting code

--- a2infinity/static/js/topic.js
+++ a2infinity/static/js/topic.js
@@ -1,15 +1,24 @@
 const dropdownHeading = document.querySelector('.dropdownHeading');
 const nonDropdownHeading = document.querySelector('.nonDropdownHeading');
-const list = document.querySelector(
+const dropdownOl = document.querySelector(
 	'.section-topics__list-items__dropdown-list'
+);
+const dropdownListItem = document.querySelector(
+	'.section-topics__list-items--dropdown'
+);
+const listIcon = document.querySelector(
+	'.section-topics__list-items__icon--dropdown'
 );
 
 dropdownHeading.addEventListener('mouseover', () => {
-	list.style.display = 'block';
+	dropdownOl.style.display = 'block';
+	dropdownOl.style.opacity = '1';
 	nonDropdownHeading.style.height = '19rem';
+   listIcon.classList.add('u-rotate-90');
 });
 
-dropdownHeading.addEventListener('mouseout', () => {
-	list.style.display = 'none';
+dropdownListItem.addEventListener('mouseleave', () => {
+	dropdownOl.style.display = 'none';
 	nonDropdownHeading.style.height = 'inherit';
+	listIcon.classList.remove('u-rotate-90');
 });
