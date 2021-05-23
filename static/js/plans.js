ea6929c7b1850d1404955bd69f7b5c432a4e0366
Ayush Chugh <ayushchugh2006@gmail.com>
2021-05-23 19:21:23 +0530

    solved subscribed height problem of plans page

--- static/js/plans.js
+++ static/js/plans.js
@@ -1,5 +1,13 @@
 const details = document.getElementsByClassName('detail-list__detail');
+const cardSide = document.getElementsByClassName('card__side');
 
 for (let detail of details) {
 	if (detail.textContent.includes('None')) detail.style.display = 'none';
+}
+
+for (let side of cardSide) {
+	if (side.textContent.includes('Subscribed')) {
+		side.classList.add('increaseSideHeight');
+		side.parentElement.classList.add('increaseCardHeight');
+	}
 }
