10597b8a0daff3f3d8e48126d0f79f4cd23bf900
Ayush Chugh <ayushchugh2006@gmail.com>
2021-05-27 19:32:12 +0530

    solved error in disableRightClick.js

--- static/js/disableRightClick.js
+++ static/js/disableRightClick.js
@@ -1,21 +1,21 @@
-document.addEventListener('contextmenu', (event) => event.preventDefault());
+document.addEventListener("contextmenu", (event) => event.preventDefault());
 
-TODO: Enable this
+TODO: Enable;
 
- document.onkeydown = function (e) {
- 	if (event.keyCode == 123) {
- 		return false;
- 	}
- 	if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
- 		return false;
- 	}
- 	if (e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
- 		return false;
- 	}
- 	if (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
- 		return false;
- 	}
- 	if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
- 		return false;
- 	}
-  };
+document.onkeydown = function (e) {
+  if (event.keyCode == 123) {
+    return false;
+  }
+  if (e.ctrlKey && e.shiftKey && e.keyCode == "I".charCodeAt(0)) {
+    return false;
+  }
+  if (e.ctrlKey && e.shiftKey && e.keyCode == "C".charCodeAt(0)) {
+    return false;
+  }
+  if (e.ctrlKey && e.shiftKey && e.keyCode == "J".charCodeAt(0)) {
+    return false;
+  }
+  if (e.ctrlKey && e.keyCode == "U".charCodeAt(0)) {
+    return false;
+  }
+};
